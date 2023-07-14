42896ecb0e5d08543c53b92e727b42766777005d
Vishnu Sharma <108869605+vishnupremium@users.noreply.github.com>
2023-07-14 14:36:03 +0530

    build

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,16 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
-
-module.exports = {
-  // Other configuration options...
   build: {
     rollupOptions: {
-      external: [
-        // Other external dependencies...
-        '@tanstack/react-query',
-      ],
+      
+      external: ['axios','@tanstack/react-query',],
     },
   },
-};
+});
+
+
