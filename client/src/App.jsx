5f708df63f3ef90a85820ac85271ebc8dc25be14
Vishnu Sharma <108869605+vishnupremium@users.noreply.github.com>
2023-07-14 14:41:53 +0530

    css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
-import '/app.css';
+import './app.css';
 import { createBrowserRouter, Navigate, Outlet, RouterProvider } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
