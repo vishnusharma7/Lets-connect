0609d5066ac18f42ed2020ed2c96037423a07ece
Vishnu Sharma <108869605+vishnupremium@users.noreply.github.com>
2023-07-15 11:30:28 +0530

    linked

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ app.use((req,res,next) => {
 })
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://let-cons.onrender.com",
 })
 );
 app.use(cookieParser())
